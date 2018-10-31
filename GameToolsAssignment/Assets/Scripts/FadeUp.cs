64a40d40da9c0e95e1abb710faf08c72518c8adc
TheIrishninjas <C17391623@mydit.ie>
2018-10-31 20:32:13 +0000

    Added Game Over Screen

---
+++ GameToolsAssignment/Assets/Scripts/FadeUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeUp : MonoBehaviour {
+    [SerializeField] private Animator fadeBoxAnim;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Fade());
+	}
+	IEnumerator Fade()
+    {
+        yield return new WaitForSeconds(0.4f);
+        fadeBoxAnim.SetBool("Fade", true);
+    }
+}
