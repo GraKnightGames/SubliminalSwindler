6e9ab7ea6d843fee473096451e2e02d4b2b6e7ae
TheIrishninjas <C17391623@mydit.ie>
2018-11-24 17:09:50 +0000

    Nav Mesh Stuff

--- GameToolsAssignment/Assets/Scripts/GuardNormalMovement.cs
+++ GameToolsAssignment/Assets/Scripts/GuardNormalMovement.cs
@@ -4,47 +4,144 @@ using UnityEngine;
 using UnityEngine.AI;
 public class GuardNormalMovement : MonoBehaviour
 {
-    private Animator m_guardAnim;
-    private Transform m_firstWayPoint;
+    private enum NPCState { CHASE, PATROL };
+    private NPCState m_NPCState;
+    private NavMeshAgent m_NavMeshAgent;
+    private int m_CurrentWaypoint;
+    private bool m_isPlayerNear;
+    private Animator m_anim;
 
-    private void Start()
+    [SerializeField] float m_FieldOfView;
+    [SerializeField] float m_ThresholdDistance;
+    [SerializeField] private Transform[] m_Waypoints;
+    [SerializeField] GameObject m_Player;
+
+    // Use this for initialization
+    void Start()
     {
-        m_guardAnim = GetComponent<Animator>();
-        m_guardAnim.SetFloat("Forward", 1);
-        m_firstWayPoint = GameObject.FindGameObjectWithTag("WP1").transform;
+        m_anim = GetComponent<Animator>();
+        m_NPCState = NPCState.PATROL;
+        m_NavMeshAgent = GetComponent<NavMeshAgent>();
+        m_CurrentWaypoint = 0;
+        m_NavMeshAgent.updatePosition = false;
+        m_NavMeshAgent.updateRotation = true;
+        HandleAnimation();
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        if(m_guardAnim.GetBool("Dying"))
+        CheckPlayer();
+        m_NavMeshAgent.nextPosition = transform.position;
+        switch (m_NPCState)
         {
-            m_guardAnim.SetFloat("Forward", 0);
-            m_guardAnim.SetFloat("Turn", 0);
-            m_guardAnim.SetLayerWeight(1, 0);
+            case NPCState.CHASE:
+                Chase();
+                break;
+            case NPCState.PATROL:
+                Patrol();
+                break;
+            default:
+                break;
         }
     }
-    private void OnTriggerEnter(Collider other)
+    void CheckPlayer()
     {
-        if(other.tag == "WP1")
+        if (m_NPCState == NPCState.PATROL && m_isPlayerNear && CheckFOV() && CheckOclusion())
         {
-            m_guardAnim.SetFloat("Forward", 0);
-            m_guardAnim.SetFloat("Turn", -1);
+            m_NPCState = NPCState.CHASE;
+            HandleAnimation();
+            return;
         }
-        if (other.tag == "WP2")
+        if (!CheckOclusion() && m_NPCState == NPCState.CHASE)
         {
-            m_guardAnim.SetFloat("Forward", -1);
-            m_guardAnim.SetFloat("Turn", 0);
+            m_NPCState = NPCState.PATROL;
         }
-        if (other.tag == "WP3")
+    }
+    void Chase()
+    {
+        m_NavMeshAgent.SetDestination(m_Player.transform.position);
+    }
+
+    bool CheckFOV()
+    {
+        Vector3 direction = m_Player.transform.position - this.transform.position;
+        Vector3 angle = (Quaternion.FromToRotation(transform.forward, direction)).eulerAngles;
+
+
+        if (angle.y > 180.0f) angle.y = 360.0f - angle.y;
+        else if (angle.y < -180.0f) angle.y = angle.y + 360.0f;
+
+
+        if (angle.y < m_FieldOfView / 2)
         {
-            m_guardAnim.SetFloat("Forward", 0);
-            m_guardAnim.SetFloat("Turn", 1);
+            return true;
         }
-        if (other.tag == "WP4")
+
+        return false;
+    }
+
+    bool CheckOclusion()
+    {
+        RaycastHit hit;
+        Vector3 direction = m_Player.transform.position - transform.position;
+        if (Physics.Raycast(this.transform.position, direction, out hit))
         {
-            m_guardAnim.SetFloat("Forward", 1);
-            m_guardAnim.SetFloat("Turn", 0);
+            if (hit.collider.gameObject == m_Player)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    void Patrol()
+    {
+        CheckWaypointDistance();
+        m_NavMeshAgent.SetDestination(m_Waypoints[m_CurrentWaypoint].position);
+    }
+
+    void CheckWaypointDistance()
+    {
+        if (Vector3.Distance(m_Waypoints[m_CurrentWaypoint].position, this.transform.position) < m_ThresholdDistance)
+        {
+            m_CurrentWaypoint = (m_CurrentWaypoint + 1) % m_Waypoints.Length;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (CheckFOV())
+            {
+                m_isPlayerNear = true;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_isPlayerNear = false;
         }
     }
    
+    private void HandleAnimation()
+    {
+        m_NavMeshAgent.nextPosition = transform.position;
+        if (m_NPCState == NPCState.CHASE)
+        {
+            m_anim.SetFloat("Forward", 2);
+        }
+        else
+        {
+            m_anim.SetFloat("Forward", 1);
+        }
+    }
 }
+        //if (m_guardAnim.GetBool("Dying"))
+        //{
+          //  m_guardAnim.SetFloat("Forward", 0);
+          //  m_guardAnim.SetFloat("Turn", 0);
+          //  m_guardAnim.SetLayerWeight(1, 0);
+       // }
