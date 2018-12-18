ed977b4f47cb6ccd6792d31321839359b6fbc2a0
TheIrishninjas <C17391623@mydit.ie>
2018-12-18 20:20:13 +0000

    Added Controls Screen

---
+++ GameToolsAssignment/Assets/Scripts/ControlScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ControlScreen : MonoBehaviour {
+    [SerializeField] private Animator m_blackBoxAnim;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Fading());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.C))
+        {
+            StartCoroutine(FadingOut());
+        }
+	}
+    IEnumerator Fading()
+    {
+        yield return new WaitForSeconds(0.4f);
+        m_blackBoxAnim.SetBool("FadeOut", true);
+    }
+    IEnumerator FadingOut()
+    {
+        m_blackBoxAnim.SetBool("FadeOut", false);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ GameToolsAssignment/Assets/Scripts/MenuToControls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuToControls : MonoBehaviour
+{
+    [SerializeField] private Animator fadeAnim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            StartCoroutine(Fading());
+        }
+    }
+    IEnumerator Fading()
+    {
+        fadeAnim.SetBool("Fading", true);
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene("Controls");
+    }
+}
